01d4b6734cc01e9a1cedb4556bf660831a44765b
malfaro rails course <malfaro.200@gmail.com>
2018-11-15 17:14:21 +0000

    create new template, action and route

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,9 @@ Rails.application.routes.draw do
   
   root to: 'pages#home'
   get 'about', to: 'pages#about'
+  
+  resources :articles
+  
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
